e7100c67f3210697154513176bac4d8df02d78ad
Ramon Echeverria gonzales <rieg@uc.cl>
2020-11-09 01:24:31 -0300

    marshmallow

---
+++ api/schemas/messages.py
@@ -0,0 +1,11 @@
+from marshmallow import Schema, fields, validate, INCLUDE
+
+class MessagesPOSTSchema(Schema):
+    message = fields.String(required=True)
+    sender = fields.Integer(required=True)
+    receptant = fields.Integer(required=True)
+    lat = fields.Float(required=True,
+        validate=validate.Range(min=-85.5, max=85.0))
+    long = fields.Float(required=True,
+        validate=validate.Range(min=-180, max=180))
+    date = fields.DateTime(format='%Y-%m-%d', required=True)
