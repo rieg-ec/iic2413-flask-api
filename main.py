9a9735a50553ba53f53a46d7b0c7a62bd9e0d43d
Ramon Echeverria gonzales <rieg@uc.cl>
2020-11-14 12:23:39 -0300

    flask env variables and change host

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from flask import Flask
 from flask_restful import Api
-
+from decouple import config
 from api.resources import (
     Messages, Message, TextSearch, Users, User
 )
 
 app = Flask(__name__)
+
+app.config.update(
+    DEBUG=config('DEBUG'),
+    ENV=config('ENV')
+)
+
 api = Api(app)
 
 api.add_resource(Messages, '/messages')
@@ -17,4 +23,4 @@ api.add_resource(User, '/users/<int:id>')
 api.add_resource(TextSearch, '/text-search')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
