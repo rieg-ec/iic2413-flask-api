0cc78bcb5e1f4260ee1d6dcb6f5b00200d4bcf1c
Ramon Echeverria gonzales <rieg@uc.cl>
2020-11-14 12:22:29 -0300

    flask :whale:

--- database/mongo-init.sh
+++ database/mongo-init.sh
@@ -14,8 +14,8 @@ db.createUser({
   }]
 });
 EOF
-mongoimport -c users -d $MONGO_DB --mode upsert --type json --jsonArray --file data/usuarios.json
-mongoimport -c messages -d $MONGO_DB --mode upsert --type json --jsonArray --file data/mensajes.json
+mongoimport -c users -d $MONGO_DB --mode upsert --type json --jsonArray --file data/files/usuarios.json
+mongoimport -c messages -d $MONGO_DB --mode upsert --type json --jsonArray --file data/files/mensajes.json
 else
     echo "env variables not provided"
     exit 403
