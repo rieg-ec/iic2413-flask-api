4a6c52cd2a0436251d6f3de30946217e10f01b29
Ramon Echeverria gonzales <rieg@uc.cl>
2020-11-17 22:56:25 -0300

    add createIndex al crear la bd

--- database/mongo-init.sh
+++ database/mongo-init.sh
@@ -18,6 +18,10 @@ db.createUser({
 EOF
 mongoimport -c users -d $MONGO_DB --mode upsert --type json --jsonArray --file data/files/usuarios.json
 mongoimport -c messages -d $MONGO_DB --mode upsert --type json --jsonArray --file data/files/mensajes.json
+mongo -u $MONGO_USER -p $MONGO_PASSWORD<<EOF
+use grupo42;
+db.messages.createIndex({'message': 'text'});
+EOF
 else
     echo "env variables not provided"
     exit 403
